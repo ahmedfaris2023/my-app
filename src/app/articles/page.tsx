b8f99c38075451cf29c42ea697bfefc8fe0b1cb4
ahmedfaris2023 <ahmadfaris080@gmail.com>
2024-10-12 20:43:52 +0300

    Data Fetching

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,5 +1,19 @@
-const ArticlePage = () => {
-  return <div>Article</div>;
+import ArticleItem from "../../../components/articles/ArticleItem";
+import { Article } from "@/utils/types";
+const ArticlePage = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const articles: Article[] = await response.json();
+  return (
+    <div>
+      <section className="container m-auto px-5">
+        <div className="flex items-center justify-center flex-wrap gap-7">
+          {articles.map((item) => (
+            <ArticleItem article={item} key={item.id} />
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default ArticlePage;
