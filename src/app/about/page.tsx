852f0b70713a5d5a78c33ac4f09cffd48d4320f7
ahmedfaris2023 <ahmadfaris080@gmail.com>
2024-10-13 14:09:47 +0300

    image componet

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,14 @@
+import Image from "next/image";
+import CloudImage from "../../../public/cloud-hosting.png";
 const AboutPage = () => {
-  return <div>About Page</div>;
+  return (
+    <section className="fix-height container m-auto">
+      <h1 className="text-3xl font-bold text-gray-800 p-5">About Page</h1>
+      <div>
+        <Image src={CloudImage} alt="cloud" width={500} height={500} priority />
+      </div>
+    </section>
+  );
 };
 
 export default AboutPage;
