16a4b61dc74c8fa7d1d6ce97634832e9d4ab8ac9
ahmedfaris2023 <ahmadfaris080@gmail.com>
2024-10-29 10:34:11 +0300

    Building API

---
+++ src/utils/data.ts
@@ -0,0 +1,22 @@
+import { Article } from "./types";
+
+export const articles: Article[] = [
+  {
+    id: 1,
+    userId: 101,
+    title: "TypeScript",
+    body: "typescript is a powerfull programming language",
+  },
+  {
+    id: 2,
+    userId: 102,
+    title: "CSharp",
+    body: "CSharp is a powerfull programming language",
+  },
+  {
+    id: 3,
+    userId: 103,
+    title: "jave",
+    body: "java is a powerfull programming language",
+  },
+];
