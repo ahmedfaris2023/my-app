24b4b9b710584cfff64fdee2bff38b278e63ab5a
ahmedfaris2023 <ahmadfaris080@gmail.com>
2024-10-29 22:43:38 +0300

    POST Route Handler

---
+++ src/utils/validationShemas.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+
+export const createArticleSchema = z.object({
+  title: z.string().min(2, "title must be more than 2 characters").max(200),
+  body: z.string().min(10),
+});
