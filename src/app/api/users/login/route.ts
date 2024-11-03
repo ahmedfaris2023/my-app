f67090b307404528aae973fd00dc2eb9780ff11c
ahmedfaris2023 <ahmadfaris080@gmail.com>
2024-11-03 19:18:21 +0300

    Login User

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,51 @@
+import { LoginUserDto } from "@/utils/dtos";
+import { loginSchema } from "@/utils/validationShemas";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/utils/db";
+import bcrypt from "bcryptjs";
+/**
+ * @method POST
+ * @route /api/users/register
+ * @desc Login User
+ * @access public
+ */
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = (await request.json()) as LoginUserDto;
+    const validation = loginSchema.safeParse(body);
+    if (!validation.success) {
+      return NextResponse.json(
+        { message: validation.error.errors[0].message },
+        { status: 400 }
+      );
+    }
+    const user = await prisma.user.findUnique({ where: { email: body.email } });
+    if (!user) {
+      return NextResponse.json(
+        { message: "please make an account , you do not have an account " },
+        { status: 400 }
+      );
+    }
+    const isPasswordMatch = await bcrypt.compare(body.password, user.password);
+    if (!isPasswordMatch) {
+      return NextResponse.json(
+        { message: "please make an account , you do not have an account" },
+        { status: 400 }
+      );
+    }
+    //@TODO -->generate JWT token
+    const token = null;
+    return NextResponse.json(
+      { message: "Authenticated", token },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "internale server error",
+      },
+      { status: 500 }
+    );
+  }
+}
--- src/utils/validationShemas.ts
+++ src/utils/validationShemas.ts
@@ -18,3 +18,9 @@ export const registerSchema = z.object({
   email: z.string().min(3).max(200).email(),
   password: z.string().min(6),
 });
+
+//login schema
+export const loginSchema = z.object({
+  email: z.string().min(3).max(200).email(),
+  password: z.string().min(6),
+});
