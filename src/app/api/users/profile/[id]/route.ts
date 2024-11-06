a7a4fc2aa03d725a337ceb9e41511858a91f8c29
ahmedfaris2023 <ahmadfaris080@gmail.com>
2024-11-06 10:52:51 +0300

    Get & Update Profile

--- src/app/api/users/profile/[id]/route.ts
+++ src/app/api/users/profile/[id]/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/utils/db";
 
 import { verifyToken } from "@/utils/verifyToken";
+import { UpdateUserDto } from "@/utils/dtos";
+import bcrypt from "bcryptjs";
 interface Props {
   params: { id: string };
 }
@@ -38,6 +40,90 @@ export async function DELETE(request: NextRequest, { params }: Props) {
         status: 403, // forbidden
       }
     );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "internal server error" },
+      { status: 500 }
+    );
+  }
+}
+
+/**
+ * @method GET
+ * @route /api/users/profile/:id
+ * @desc Get Profile by id
+ * @access private
+ */
+export async function GET(request: NextRequest, { params }: Props) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: parseInt(params.id) },
+      select: {
+        id: true,
+        email: true,
+        username: true,
+        createAt: true,
+        isAdmin: true,
+      },
+    });
+    if (!user) {
+      return NextResponse.json({ message: "user not found" }, { status: 404 });
+    }
+    const userFromToken = verifyToken(request);
+    if (userFromToken === null || userFromToken.id !== user.id) {
+      return NextResponse.json(
+        {
+          message: "youn are not allowed, access denied",
+        },
+        { status: 403 }
+      );
+    }
+    return NextResponse.json(user, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "internal server error" },
+      { status: 500 }
+    );
+  }
+}
+
+/**
+ * @method PUT
+ * @route /api/users/profile/:id
+ * @desc Update Profile
+ * @access private
+ */
+
+export async function PUT(request: NextRequest, { params }: Props) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: parseInt(params.id) },
+    });
+    if (!user) {
+      return NextResponse.json({ message: "user not found" }, { status: 404 });
+    }
+    const userFromToken = verifyToken(request);
+    if (userFromToken === null || userFromToken.id !== user.id) {
+      return NextResponse.json(
+        { message: "you are not allowed , access denied" },
+        { status: 403 }
+      );
+    }
+    const body = (await request.json()) as UpdateUserDto;
+    if (body.password) {
+      const salt = await bcrypt.genSalt(10);
+      body.password = await bcrypt.hash(body.password, salt);
+    }
+    const updatedUser = await prisma.user.update({
+      where: { id: parseInt(params.id) },
+      data: {
+        username: body.username,
+        email: body.email,
+        password: body.password,
+      },
+    });
+    const { password, ...other } = updatedUser;
+    return NextResponse.json({ ...other }, { status: 200 });
   } catch (error) {
     return NextResponse.json(
       { message: "internal server error" },
